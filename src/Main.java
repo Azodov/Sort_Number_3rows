0cf7a9410898c67d6fc7b76910b5eea25a97c027
Donik11 <botirxonn@gmail.com>
2022-02-28 17:25:05 +0300

    sort n number with 3 rows in 1 for

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("N ni kiriting: ");
+        int n = scanner.nextInt();
+        int k = n/3;
+
+        for (int i=0; i<=n; i++) {
+            if (i%3 ==0) {
+                System.out.println("");
+            }
+            System.out.print(i+" ");
+        }
+    }
+}
